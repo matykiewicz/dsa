46d155a5220351937de0db79391e8e3212438849
pawelm <pawelm>
2016-08-02 21:26:47 -0700

    Create statement

--- main_hta.c
+++ main_hta.c
@@ -19,6 +19,40 @@ hta *create_hta ( int size ) {
   return(h);
 }
 
+// -- http://burtleburtle.net/bob/hash/doobs.html
+// -- http://www.cse.yorku.ca/~oz/hash.html
+
+unsigned long hash ( unsigned char *str ) {
+  unsigned long hash = 5381;
+  int c;
+  while ( c = *str++ ) {
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  }
+  return hash;
+}
+
+void add_hta ( hta *this_hta, void *key, void *value ) {
+}
+
+//int hash_index (hash_t *h, void *key) {
+//  int i = (int) key % h->size;
+//  while ( h->keys[i] && h->keys[i] != key ) {
+//    i = (i + 1) % h->size;
+//  }
+//  return i;
+//}
+//
+//void hash_insert (hash_t *h, void *key, void *value) {
+//  int i = hash_index(h, key);
+//  h->keys[i] = key;
+//  h->values[i] = value;
+//}
+//
+//void *hash_lookup (hash_t *h, void *key) {
+//  int i = hash_index(h, key);
+//  return h->values[i];
+//}
+
 int main ( ) {
 
   // - - create
