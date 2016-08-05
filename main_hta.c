de1bf420837f3aa71d2a4d5929f9e69dbd8180bc
pawelm <pawelm>
2016-08-04 22:28:35 -0700

    More algorithms and DS

--- main_hta.c
+++ main_hta.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <limits.h>
+#include <string.h>
 
 typedef struct {
   int size;
@@ -29,22 +30,51 @@ hta *create_hta ( int size ) {
 
 // -- http://burtleburtle.net/bob/hash/doobs.html
 // -- http://www.cse.yorku.ca/~oz/hash.html
+// -- http://www.partow.net/programming/hashfunctions/
 
-unsigned long hash_str ( unsigned char *str ) {
-  double hash = 0;
-  double hashhash = 1;
-  int c;
-  int j=2;
+// unsigned long hash_str ( unsigned char *str, int size ) {
+//   double hash = 0.1234;
+//   int c;
+//   int j=1;
+//   int index;
+//   while ( (c = *str++) ) {
+//     hash = (hash + ((double) (c-64))/26)/2;
+//     for ( int i = 0; i < j; i++ ) { 
+//       hash = 3.999999*hash*(1-hash);
+//     }
+//     j++;
+//   }
+//   //printf("%.5f\n",hash);
+//   index = (int) (hash*size);
+//   return(index);
+// }
+
+int hash_str ( unsigned char *str, int size ) {
+  unsigned long hash = 0;
+  unsigned long c;
+  int index = 0;
   while ( (c = *str++) ) {
-    hash = ((double) c)/255;
-    for ( int i = 0; i < j; i++ ) { 
-      hash = 3.9*hash*(1-hash);
-    }
-    j++;
-    hashhash = 10*hashhash + 300*hash;
+    hash += (hash<<5) + c;
   }
-  return((unsigned long) hashhash);
+  index = (int) (hash&size);
+  return(index);
 }
+
+//unsigned long hash_str ( unsigned char *str ) {
+//  double hash = 0;
+//  double hashhash = 1;
+//  int c;
+//  int j=2;
+//  while ( (c = *str++) ) {
+//    hash = ((double) c)/255;
+//    for ( int i = 0; i < j; i++ ) { 
+//      hash = 3.9*hash*(1-hash);
+//    }
+//    j++;
+//    hashhash = 10*hashhash + 300*hash;
+//  }
+//  return((unsigned long) hashhash);
+//}
 
 // unsigned long hash_str ( unsigned char *str ) {
 //   unsigned long hash = 0;
@@ -79,20 +109,18 @@ unsigned long hash_str ( unsigned char *str ) {
 //}
 
 void add_hta ( hta *this_hta, void *key, int value ) {
-  unsigned long i = hash_str((unsigned char *) key);
-  int j = (int) (i & this_hta->size);
+  int i = hash_str((unsigned char *) key,this_hta->size);
   this_hta->inserts++;
-  if ( this_hta->keys[j] == NULL ) {
-    this_hta->keys[j] = key;
-    this_hta->values[j] = value;
-  } else {
+  if ( this_hta->keys[i] == NULL ) {
+    this_hta->keys[i] = key;
+    this_hta->values[i] = value;
+  } else if ( strcmp(key,this_hta->keys[i]) != 0 ) {
     this_hta->collisions++;
   }
 }
 
 int find_hta (hta *this_hta, void *key) {
-  unsigned long i = hash_str((unsigned char *) key);
-  int j = (int) (i & this_hta->size);
+  int i = hash_str((unsigned char *) key,this_hta->size);
   return(this_hta->values[i]);
 }
 
@@ -103,12 +131,12 @@ int main ( ) {
   int r = 0;
   int size = ((int) 0xFFFFF); printf("size %d\n",size);
   char *key = NULL;
-  srand(time(NULL));
+  srand(123456);
   // - - create
   hta *this_hta = create_hta(size);
   // - - add 
-  for ( i = 0; i < (int) (size/5); i++ ) {
-    r = rand()%10+4;
+  for ( i = 0; i < (int) (10000); i++ ) {
+    r = rand()%11+4;
     key = (char *) calloc((r+1),sizeof(char));
     for ( j = 0; j < r; j++ ) {
       key[j] = (char) 65+(rand()%24);
@@ -117,8 +145,24 @@ int main ( ) {
     add_hta(this_hta,key,r);
   }
   printf("%s %d\n",this_hta->keys[1],this_hta->values[1]);
-  printf("Collisions %f %d",(double) (((float) this_hta->collisions)/((float) this_hta->inserts)),this_hta->inserts);
-
+  printf("Collisions %f %d\n",(double) (((double) this_hta->collisions)/((double) this_hta->inserts)),this_hta->inserts);
+  // - - find
+  srand(123456);
+  int not_found = 0;
+  for ( i = 0; i < (int) (10000); i++ ) {
+    r = rand()%11+4;
+    key = (char *) calloc((r+1),sizeof(char));
+    for ( j = 0; j < r; j++ ) {
+      key[j] = (char) 65+(rand()%24);
+    }
+    key[r] = '\0';
+    int f = find_hta(this_hta,key);
+    if ( f != r ) {
+      not_found++;
+      printf("%s %d %d\n",key,r,f);
+    }
+  }
+  printf("Not found %f %d\n",(double) (((double) not_found)/((double) this_hta->inserts)),not_found);
   // - - destroy
   for ( i = 0; i < this_hta->size; i++ ) {
     free(this_hta->keys[i]);
@@ -127,7 +171,6 @@ int main ( ) {
   free(this_hta->keys);
   free(this_hta);
   // - - pause
-  getchar();
   return(1);
 
 }
