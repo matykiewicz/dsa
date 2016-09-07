63e93d96acd51894f1984c925a9f02df838394b8
pawelm <pawelm>
2016-09-06 22:14:41 -0700

    More kNN checking for sample size ... and other puzzles

--- check_ss_with_knn.R
+++ check_ss_with_knn.R
@@ -1,11 +1,26 @@
+## ----
 
+create_2d_data <<- function ( n_small = 20, n_big = 10, d_small = 0.15, d_big = 0.45 ) {
+  m_big   <- matrix(round(rnorm(n_big*n_big,d_big,0.05)),n_big,n_big);
+  m_final <- NULL;
+  class_lab <- NULL;
+  for ( i in 1:nrow(m_big) ) {
+    for ( j in 1:ncol(m_big) ) {
+      if ( m_big[i,j] ) {
+	m_final <- rbind(m_final,cbind(rnorm(n_small,i*1,d_small),rnorm(n_small,j*1,d_small)));
+	class_lab <- c(class_lab,rep(c('A','B')[round(rnorm(1,d_big+0.05,0.15))+1],n_small));
+      }
+    }
+  }
+  print(paste('Dim',nrow(m_final),ncol(m_final)));
+  m_final <- cbind(as.data.frame(m_final),CLASS=class_lab);
+  return(m_final);
+}
 
-n <- 100;
-x1 <- cbind(rnorm(n,1,1));
-x2 <- cbind(rnorm(n,1.5,1));
-cl <- c(rep('a',n),rep('b',n));
-dis <- as.matrix(dist(rbind(x1,x2)));
-
-knn <- t(apply(dis,1,function(x){cl[sort(x,ind=T,dec=F)$ix]}));
-knn_ratio <- t(apply(knn,1,function(x){sapply(seq(x),function(y){max(table(x[1:y]))/y  })}));
-
+check_ratio <- function ( data_matrix ) {
+  cl <- data_matrix[,3];
+  dis <- as.matrix(dist(data_matrix[,1:2]));
+  knn <- t(apply(dis,1,function(x){cl[sort(x,ind=T,dec=F)$ix]}));
+  knn_ratio <- t(apply(knn,1,function(x){sapply(seq(x),function(y){max(table(x[1:y]))/y  })}));
+  return(knn_ratio);
+}
--- couple_other_problems.R
+++ couple_other_problems.R
@@ -1,4 +1,4 @@
-## ==
+## ====
 
 # a[i] = a[a[i]];
 
@@ -11,7 +11,7 @@ a_out = c(1,0,3,2)+1; # 2,1,4,3
 # a[4] = a[1] = 3
 # a[1] = 3 + 5*2
 
-rep <- function ( a_in ) {
+repl <- function ( a_in ) {
   for ( i in seq(a_in) ) {
     a_in[i] = a_in[i] + (a_in[a_in[i]]%%(length(a_in)+1))*(length(a_in)+1);
   }
@@ -21,6 +21,73 @@ rep <- function ( a_in ) {
   }
   return(a_in);
 }
+print(a_in);
+print(repl(a_in));
 
+## ====
 
+a_in = c(2,3,1,4);
+a_out = c(12,8,24,6);
+
+# 2*3*1*4 = 24
+
+mult <- function ( a_in ) {
+  mul <- 1;
+  for ( i in seq(a_in) ) {
+    mul <- mul*a_in[i];
+  }
+  for ( i in seq(a_in) ) {
+    a_in[i] <- mul/a_in[i]
+  }
+  return(a_in);
+}
+print(a_in);
+print(mult(a_in));
+
+## ====
+
+a_in = c(1,3,5,18);
+
+# 8 -> 1, 1+3, 1+3+5, 3+5
+# 9 -> 1, 1+3, 1+3+5
+# 10 -> 1, 1+3, 1+3+5, 1+3+5+18, 3+5+18, 5+18, 18
+# 23 -> 1, 1+3, 1+3+5, 1+3+5+18, 3+5+18, 5+18
+
+IsThereSub <- function ( ar, target ) {
+  left <- 1;
+  right <- 1;
+  Sum <- 0;
+  len <- length(ar);
+  if ( target < min(ar) ) {
+    return(FALSE);
+  }
+  while ( right <= len || left < len ) {
+    if ( Sum < target && right <= len ) {
+      Sum <- Sum + ar[right];
+      right <- right + 1;
+    }
+    if ( Sum > target && left <= len ) {
+      Sum <- Sum - ar[left];
+      left <- left + 1;
+    }
+    if ( Sum < target && right > len && left == 1 ) {
+      return(FALSE);
+      break;
+    }
+    if ( Sum == target ) {
+      return(TRUE);
+      break;
+    }
+  }
+  return(FALSE);
+}
+print(a_in);
+print(IsThereSub(a_in,8));
+print(IsThereSub(a_in,9));
+print(IsThereSub(a_in,10));
+print(IsThereSub(a_in,40));
+print(IsThereSub(a_in,18));
+print(IsThereSub(a_in,18+5));
+print(IsThereSub(a_in,18+5+3));
+print(IsThereSub(a_in,0));
 
